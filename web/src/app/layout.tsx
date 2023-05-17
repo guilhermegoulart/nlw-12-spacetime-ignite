2eb92174514f8688568dedce5ee8d023db2a6393
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-05-17 13:45:12 -0300

    first commit of the mobile and server directories, and the second step of web directorie

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjure,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baiJamjuree = BaiJamjure({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo construida com React, Next.js, TailwindCSS and Typescript',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
